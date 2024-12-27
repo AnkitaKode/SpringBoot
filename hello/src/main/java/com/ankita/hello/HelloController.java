2442887a30798d8dba3bcd9baf332fcfe4784c50
Ankita Kumari <ankita00iv@gmail.com>
2024-12-27 23:10:42 +0530

    initial commit

---
+++ hello/src/main/java/com/ankita/hello/HelloController.java
@@ -0,0 +1,15 @@
+package com.ankita.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@RequestMapping("/hello")
+
+    public String Hello() {
+
+
+        return "Hello, World!";
+}
+}
